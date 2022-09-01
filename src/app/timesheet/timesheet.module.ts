79e7f798cccc3b61e6dd9ca9e1637974dec40b9c
nsheik15 <nsheik15@gmail.com>
2022-09-01 14:32:02 +0530

    Initial Project Setup

---
+++ src/app/timesheet/timesheet.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TimesheetRoutingModule } from './timesheet-routing.module';
+import { TimesheetComponent } from './timesheet/timesheet.component';
+
+
+@NgModule({
+  declarations: [
+    TimesheetComponent
+  ],
+  imports: [
+    CommonModule,
+    TimesheetRoutingModule
+  ]
+})
+export class TimesheetModule { }
