a8da3671743d641cb17b7c245c11c5ac4da0e414
nsheik15 <nsheik15@gmail.com>
2022-09-03 22:45:42 +0530

    Login page UI Design Development

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { PrimengModule } from '../primeng/primeng.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    AuthenticationRoutingModule,
+    PrimengModule
   ]
 })
 export class AuthenticationModule { }
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import {TableModule} from 'primeng/table';
 import {ButtonModule} from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import {CardModule} from 'primeng/card';
+import {CheckboxModule} from 'primeng/checkbox';
 
 const PrimengComponents = [
   TableModule,
   ButtonModule,
-  InputTextModule
+  InputTextModule,
+  CardModule,
+  CheckboxModule
 ]
 
 @NgModule({
