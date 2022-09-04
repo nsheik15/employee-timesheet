4552b2be42d64c71f2ad0b83c020465e08694618
nsheik15 <nsheik15@gmail.com>
2022-09-04 16:02:41 +0530

    Register UI Design Development & Functionality Integration

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+    this.initRegisterForm();
+  }
+
+  initRegisterForm() {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      organization: ['', [Validators.required]],
+      designation: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]],
+      isAdmin: [false]
+    });
+  }
+
+  register() {
+    if(this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      this.router.navigate(['/login']);
+    }
   }
 
 }
